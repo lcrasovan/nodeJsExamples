8cddd8230e354fee9077ad93d78f3cbc15dce9c5
lucian crasovan <l.crasovan@resto-in.com>
2016-10-03 12:11:15 +0200

    read commandline parameters with gulp and pass them to tasks

--- ftpDownloader.js
+++ ftpDownloader.js
@@ -2,28 +2,36 @@ var ftpClient = require('ftp-client'),
     fs = require('fs'),
     config = JSON.parse(fs.readFileSync(__dirname + '/config.json')),
     moment = require('moment'),
-    port = config.ftp.port,
-    host = config.ftp.host,
-    ftpUserName = config.ftp.userName,
-    ftpPassword = config.ftp.password,
+    minimist = require('minimist'),
+    port = config['ftp']['port'],
+    host = config['ftp']['host'],
+    ftpUserName = config['ftp']['userName'],
+    ftpPassword = config['ftp']['password'],
+    ftpFolder = config['ftp']['srcFolder'],
+    localDestinationFolder = config['ftp']['destinationFolderPath'],
     config = {
         host: host,
         port: port,
         user: ftpUserName,
         password: ftpPassword
     },
-    ftpFolder = config.ftp.srcFolder,
-    localDestinationFolder = config.ftp.destinationFolderPath, 
-    options = {
+    knownOptions = {
+        string: 'daysBefore',
+        default: { daysBefore: 1 },
         logging: 'basic'
     },
+    options = minimist(process.argv.slice(2), knownOptions),
     client = new ftpClient(config, options);
 
 client.connect(function () {
-    var yesterdayDatePattern = moment().subtract(1, 'days').format('YYYY\-MM\-DD');
+
+    console.log(options);
+
+    var datePattern = moment().subtract(options.daysBefore, 'days').format('YYYY\-MM\-DD');
+
     client.download(ftpFolder, localDestinationFolder, {
         overwrite: 'all',
-        fileNamePattern: yesterdayDatePattern
+        fileNamePattern: datePattern
     }, function (result) {
         console.log(result);
     });
